5f4c81584bba0e47cbb86e25bb000a9cf4e4f89d
Cristian Meneses <c.meneses@outlook.cl>
2021-12-17 00:50:00 -0300

    Agregar archivos de proyecto.

---
+++ Colecciones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates and initializes a new ArrayList.
+            ArrayList Lista = new ArrayList();
+            Lista.Add("Crema");
+            Lista.Add("Pasta Dental");
+            Lista.Add("Perfume");
+            Lista.Add("Peineta");
+            Lista.Add("Cera");
+
+            // Displays the properties and values of the ArrayList.
+            Console.WriteLine("Lista");
+            Console.WriteLine("Cntidad de elementos:{0}", Lista.Count);
+            Console.WriteLine("Largo del array: {0}", Lista.Capacity);
+            Console.WriteLine("Elementos:");
+            ListarValores(Lista);
+        }
+        public static void ListarValores(IEnumerable miLista)
+        {
+            foreach (Object obj in miLista)
+                Console.Write("{0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
